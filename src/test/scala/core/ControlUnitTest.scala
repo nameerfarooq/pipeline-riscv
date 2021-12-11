53bf66d3948e57cb297b5fb3cf9d88252c74185b
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-12 00:42:53 +0500

    controlunit tested

--- src/test/scala/core/ControlUnitTest.scala
+++ src/test/scala/core/ControlUnitTest.scala
@@ -7,16 +7,16 @@ import chiseltest._
 class ControlUnitTest extends FreeSpec with ChiselScalatestTester{
     "control unit test" in {
         test(new ControlUnit()){c=>
-        c.io.Opcode.poke("b0010011".U)
+        c.io.Opcode.poke("b1100111".U)
         c.clock.step(100)
         c.io.Branch.expect(0.U)    
-        c.io.RegWrite.expect(1.U)    
-        c.io.Immediate.expect(1.U)    
+        c.io.RegWrite.expect(0.U)    
+        c.io.Immediate.expect(0.U)    
         c.io.Load.expect(0.U)    
         c.io.Store.expect(0.U)    
         c.io.UType.expect(0.U)    
         c.io.Auipc.expect(0.U)    
-        c.io.Jalr.expect(0.U)    
+        c.io.Jalr.expect(1.U)    
         c.io.jal.expect(0.U)    
         }
     }
